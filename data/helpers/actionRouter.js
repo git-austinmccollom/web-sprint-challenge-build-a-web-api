dc75bb9fddb2cda4a06bd21828f41ead4dab50b3
git-austinmccollom <austinmccollom@gmail.com>
2020-10-02 11:15:13 -0500

    actionRouter and gets

---
+++ data/helpers/actionRouter.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const postFunctions = require("./projectModel");
+const actionFunctions = require("./actionModel");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    actionFunctions.get()
+    .then( dbRes => {
+        res.status(200).json(dbRes);
+    })
+    .catch( dbErr => {
+        res.status(500).json(dbErr);
+    })
+})
+
+router.get("/:id", (req, res) => {
+    actionFunctions.get(req.params.id)
+    .then( dbRes => {
+        res.status(200).json(dbRes);
+    })
+    .catch( dbErr => {
+        res.status(500).json(dbErr);
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const server = express();
 const morgan = require("morgan");
 const logger = morgan("combined");
 const projectRouter = require("./data/helpers/projectRouter");
+const actionRouter = require("./data/helpers/actionRouter");
 
 server.use(express.json());
 server.use(logger);
 
 server.use("/api/projects", projectRouter);
+server.use("/api/actions", actionRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Hello World</h2>`);
